0d464a956042f7a518c93056db31e013a6409f99
Jobson Oliveira <jobsondeveloper@gmail.com>
2024-12-16 11:08:11 -0300

    Projeto iniciado, foi criado o arquivo para utilização da API. Foi criado o arquivo Layout.

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Layout = ({ children }) => {
+    return (
+        <section>
+            <header>Header</header>
+            {children}
+        </section>
+    )
+}
